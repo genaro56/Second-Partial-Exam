3708eb14bccdad5591f20751e84bcc20d9020fbc
genaromartinez <genaro.mtz1998@gmail.com>
2020-05-08 08:20:51 -0500

    add changes

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,8 +1,31 @@
 const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
+
+// id: uuid.v4(), // This is a String type holding a uuid
+// name: String,
+// num_players: Number
 /* Your code goes here */
+const sport = mongoose.Schema({
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    num_players: { type: Number, required: true }
+});
+
+const sportsCollection = mongoose.model('sports', sport);
+
+const Sports = {
+    deleteSport: (id) => {
+        return sportsCollection
+            .deleteOne({id})
+                .then(res => {
+                    console.log("sport deleted");
+                    return res;
+                })
+                .catch(err => {return err});
+    }
+}
 
 module.exports = {
-    
+    Sports
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -3,12 +3,43 @@ const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
-
+const Sports = require('./models/sport-model');
 const app = express();
 
+// let sports = [
+
+// ]
 
 /* Your code goes here */
-
+app.delete('/sports/delete', (reqs, res) => {
+    let id = reqs.body.id;
+    let sportId = reqs.query.id;
+    if(!id) {
+        res.statusMessage = "no id was sent in the body"
+        return res.statusCode(406).end();
+    }
+    if(!sportId) {
+        res.statusMessage = "no id was sent in query parameters"
+        return res.statusCode(406).end();
+    }
+    
+    if(sportId != id) {
+        res.statusMessage = "the ids dont match";
+        return res.statusCode(409).end();
+    }
+    Sports.deleteSport(sportId)
+        .then(res => {
+            if(res.deletedCount < 1) {
+                err.statusMessage = "the id doesnt exist";
+                return res.statusCode(404).end();
+            }
+            res.statusMessage = "sport deleted";
+            return res.statusCode(204).end();
+        }).catch(err => {
+            err.statusMessage = "server error" + err;
+            return err.statusCode(400)
+        })
+})
 
 app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
